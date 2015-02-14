0c14f72b47bda08fd5120d8edbceb1950c8ad2fb
aoitan <aoi@nagatsuki-do.net>
2015-02-14 23:57:51 +0900

    update

--- javascripts/map_view_script.js
+++ javascripts/map_view_script.js
@@ -77,6 +77,7 @@ function makeMarker(file)
 var dispatcher = {
   'current': function (params) {
     console.log('current: ' + JSON.stringify(params));
+    mapView(params.coords.latitude, params.coords.longitude);
   }
 };
 
