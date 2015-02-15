8b8dc0e3aef3f5852ee65bd05937b235e40fff00
aoitan <aoi@nagatsuki-do.net>
2015-02-15 10:39:38 +0900

    change zoom ratio

--- javascripts/map_view_script.js
+++ javascripts/map_view_script.js
@@ -9,7 +9,7 @@ function map_view_loading() {
 
 function mapView(latitude, longitude) {
   var mapOptions = {
-    zoom: 8,
+    zoom: 12,
     center: new google.maps.LatLng(latitude, longitude),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
